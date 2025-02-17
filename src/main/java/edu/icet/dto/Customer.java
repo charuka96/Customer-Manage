8044563220ec15c8ba5bdce2924867b4e0f7d615
charuka96 <chaukathisaru96@gmail.com>
2025-02-17 08:16:19 +0530

    add nic to dto

--- src/main/java/edu/icet/dto/Customer.java
+++ src/main/java/edu/icet/dto/Customer.java
@@ -1,2 +1,22 @@
-package edu.icet.dto;public class Customer {
+package edu.icet.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.yaml.snakeyaml.scanner.ScannerImpl;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+
+private Integer id;
+private String name;
+private String email;
+private String address;
+private String nic;
+
 }
