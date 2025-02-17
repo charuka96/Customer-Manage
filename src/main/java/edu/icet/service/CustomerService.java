b9a5792112761469dd41d0aa2e8a83d7e99c9663
charuka96 <chaukathisaru96@gmail.com>
2025-02-17 08:20:23 +0530

    remove nic

--- src/main/java/edu/icet/service/CustomerService.java
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -1,2 +1,13 @@
-package edu.icet.service;public interface CustomerService {
+package edu.icet.service;
+
+import edu.icet.dto.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    void addCustomer(Customer customer);
+    void deleteCustomer(Integer id);
+    Customer serchById(Integer id);
+    List<Customer>getAll();
+    void updateCustomer(Customer customer);
 }
